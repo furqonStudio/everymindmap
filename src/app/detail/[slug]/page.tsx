a3bd709ff8a97688e3c85fb793d44793228e0e07
furqonstudio <furqonstudio@gmail.com>
2024-12-02 11:44:18 +0700

    fix: slug promise in detail page

--- src/app/detail/[slug]/page.tsx
+++ src/app/detail/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import FigmaFrame from '@/components/FigmaFrame'
-import { notFound } from 'next/navigation'
 
-export default function DetailPage({ params }: { params: { slug: string } }) {
+export default async function DetailPage(props: {
+  params: Promise<{ slug: string }>
+}) {
+  const params = await props.params
   return (
     <div>
       <h1 className="p-4 absolute text-2xl font-bold mb-4 capitalize">
